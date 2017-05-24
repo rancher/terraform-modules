d58f78d264216ca8ade2573299cb82ab59a1fb59
William Jimenez <william@rancher.com>
2017-05-24 16:56:25 -0700

    refactor to use GCE instance group manager

--- example_ha/gce/main.tf
+++ example_ha/gce/main.tf
@@ -9,31 +9,31 @@ module "gce_compute" {
   source = "../../modules/gce/compute"
 
   name = "test"
-  machine_type = ""
-  zone = ""
-  disk_image = ""
+  machine_type = "n1-standard-1"
+  zone = "us-central1-f"
+  disk_image = "ubuntu-os-cloud/ubuntu-1604-lts"
   server_count = "1"
   compute_count = "1"
   service_account_scopes = []
 }
 
-provider "rancher" {
-  api_url = "{$gce_compute.google_compute_instance.rancher_server.network_interface.0.access_config.0.assigned_nat_ip}"
-}
-
-resource "rancher_environment" "default" {
-  //depends_on = ["gce_compute.google_compute_instance.rancher_server"]
-  name = "env1"
-  description = "The Default environment"
-  orchestration = "cattle"
-}
-
-module "gce_database" {
-  source = "../../modules/gce/database"
-
-  name = "test"
-  region = ""
-  database_tier = ""
-  disk_size = "1"
-  disk_type = "foo"
-}
+# provider "rancher" {
+#   api_url = "{$gce_compute.google_compute_instance.rancher_server.network_interface.0.access_config.0.assigned_nat_ip}"
+# }
+#
+# resource "rancher_environment" "default" {
+#   //depends_on = ["gce_compute.google_compute_instance.rancher_server"]
+#   name = "env1"
+#   description = "The Default environment"
+#   orchestration = "cattle"
+# }
+#
+# module "gce_database" {
+#   source = "../../modules/gce/database"
+#
+#   name = "test"
+#   region = ""
+#   database_tier = ""
+#   disk_size = "1"
+#   disk_type = "foo"
+# }
--- modules/gce/compute/main.tf
+++ modules/gce/compute/main.tf
@@ -1,19 +1,63 @@
 // Rancher server node
-resource "google_compute_instance" "rancher_server" {
-  count        = "${var.server_count}"
+resource "google_compute_instance_group_manager" "rancher-servers" {
+  name        = "${var.name}"
+  description = "Terraform test instance group manager"
+
+  base_instance_name = "${var.name}-server"
+  instance_template  = "${google_compute_instance_template.rancher-servers.self_link}"
+  update_strategy    = "NONE"
+  zone               = "us-central1-a"
+
+  target_pools = ["${google_compute_target_pool.rancher-servers.self_link}"]
+  target_size  = "${var.server_count}"
+
+  named_port {
+    name = "rancher-api"
+    port = 8888
+  }
+}
+
+resource "google_compute_health_check" "rancher-servers" {
+  name = "rancher-servers"
+
+  timeout_sec        = 1
+  check_interval_sec = 1
+
+  tcp_health_check {
+    port = "8080"
+  }
+}
+
+
+resource "google_compute_target_pool" "rancher-servers" {
+  name = "test"
+  depends_on = ["google_compute_health_check.rancher-servers"]
+
+  # Need to revisit why this isn't working, it tries to lookup an HTTP load balancer
+  # health_checks = [
+  #   "${google_compute_health_check.rancher-servers.name}",
+  # ]
+}
+
+resource "google_compute_instance_template" "rancher-servers" {
   name         = "${var.name}-server"
+  description = "template description"
+
+  tags = ["foo", "bar"]
   machine_type = "${var.machine_type}"
-  zone         = "${var.zone}"
+  instance_description = "description assigned to instances"
+  can_ip_forward       = false
 
-  tags = ["rancher_server"]
-
-  disk {
-    image = "${var.disk_image}"
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
   }
 
+
   disk {
-    type    = "local-ssd"
-    scratch = true
+    source_image = "${var.disk_image}"
+    auto_delete  = true
+    boot         = true
   }
 
   network_interface {
@@ -27,44 +71,15 @@ resource "google_compute_instance" "rancher_server" {
   metadata {}
 
   metadata_startup_script = "${var.server_metadata_startup_script}"
-  //metadata_startup_script = "docker run -d --restart=unless-stopped -p 8080:8080 --name=rancher-server rancher/server:stable"
+
 
   service_account {
     scopes = ["${var.service_account_scopes}"]
   }
 }
 
-// Rancher compute node
-resource "google_compute_instance" "rancher_compute" {
-  count        = "${var.compute_count}"
-  name         = "${var.name}"
-  machine_type = "${var.machine_type}"
-  zone         = "${var.zone}"
-
-  tags = ["rancher_server"]
-
-  disk {
-    image = "${var.disk_image}"
-  }
-
-  disk {
-    type    = "local-ssd"
-    scratch = true
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral IP
-    }
-  }
-
-  metadata {}
-
-  metadata_startup_script = "${var.compute_metadata_startup_script}"
-
-  service_account {
-    scopes = ["${var.service_account_scopes}"]
-  }
+resource "google_compute_forwarding_rule" "rancher-servers" {
+  name       = "test"
+  target     = "${google_compute_target_pool.rancher-servers.self_link}"
+  port_range = "80"
 }
