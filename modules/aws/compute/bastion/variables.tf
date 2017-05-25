c295e2d5591d0148cd23cc72fae7d9e313a344d2
Bill Maxwell <bill@rancher.com>
2017-05-24 17:06:19 -0700

    added bastion host modules

---
+++ modules/aws/compute/bastion/variables.tf
@@ -0,0 +1,15 @@
+variable "name" {}
+
+variable "vpc_id" {}
+
+variable "bastion_instance_type" {}
+
+variable "ssh_key_name" {}
+
+variable "image_id" {}
+
+variable "subnet_ids" {}
+
+variable "eip" {}
+
+variable "userdata" {}
