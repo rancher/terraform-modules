22af49143bb5e0ccfe06177996da286b202779da
William Jimenez <william@rancher.com>
2017-06-22 14:00:08 +1000

    removing Ubuntu related stuff from metadata scripts

--- modules/gce/compute/metadata_scripts/server.sh
+++ modules/gce/compute/metadata_scripts/server.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
 set -x
 
-# This version assumes ubuntu
-
-# Set Hostname
-uuid="$(cat /sys/class/net/*/address | head -n 1 |sed -r 's/[:]+/-/g')"
-node_hostname=rancher-node-$uuid
-hostnamectl set-hostname $node_hostname
-echo "127.0.0.1  $node_hostname" >> /etc/hosts
-
-# Setup Docker + Rancher
-apt-get update && apt -y install docker.io=1.12.6-0ubuntu1~16.04.1
 docker run -d --restart=unless-stopped -p 8080:8080 --name=rancher-server rancher/server:stable
