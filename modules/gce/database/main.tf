aab116adebcf9cdcbbb500a5b3472a53939cb1f9
William Jimenez <william@rancher.com>
2017-08-07 16:47:12 -0700

    create database in database instance

--- modules/gce/database/main.tf
+++ modules/gce/database/main.tf
@@ -8,8 +8,21 @@ resource "google_sql_database_instance" "master" {
     tier = "${var.db_tier}"
     disk_size = "${var.disk_size}"
     disk_type = "${var.disk_type}"
-  }
+    ip_configuration {
+            ipv4_enabled = true
+            authorized_networks {
+                name = "default"
+                value = "0.0.0.0/0"
+              }
+          }
+      }
 }
+
+resource "google_sql_database" "master" {
+    name = "cattle"
+    instance = "${google_sql_database_instance.master.name}"
+}
+
 
 resource "google_sql_user" "rancher" {
   name     = "${var.db_user}"
