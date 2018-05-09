771de64c7f8b0e1f9f16431571506fd51f12507a
William Jimenez <wjimenez5271@gmail.com>
2018-05-08 17:23:06 -0700

    fixing relative path for example (#16)

--- example_standalone/do/main.tf
+++ example_standalone/do/main.tf
@@ -9,7 +9,7 @@ variable "ssh_keys" {
 }
 
 module "digital-ocean" {
-  source = "../modules/do/compute"
+  source = "../../modules/do/compute"
   node_count = 1
   digitalocean_token = "${var.digitalocean_token}"
   docker_cmd = "docker run -d --restart=unless-stopped -p 8080:8080 rancher/server:${var.rancher_version_tag}"
