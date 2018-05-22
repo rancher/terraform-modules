49655bbdfa03fe19f341175e40ac5de7aeb3da83
Jay Gentile <jcgentile01@gmail.com>
2018-05-22 09:20:38 -0700

    Modifying name and id outputs to use splat syntax
    Fix for the following errors while running terraform plan:
    
    Warning: output "name": must use splat syntax to access aws_autoscaling_group.alb attribute "name", because it has "count" set; use aws_autoscaling_group.alb.*.name to obtain a list of the attributes across all instances
    
    
    
    Warning: output "name": must use splat syntax to access aws_autoscaling_group.elb attribute "name", because it has "count" set; use aws_autoscaling_group.elb.*.name to obtain a list of the attributes across all instances
    
    
    
    Warning: output "id": must use splat syntax to access aws_autoscaling_group.alb attribute "id", because it has "count" set; use aws_autoscaling_group.alb.*.id to obtain a list of the attributes across all instances
    
    
    
    Warning: output "id": must use splat syntax to access aws_autoscaling_group.elb attribute "id", because it has "count" set; use aws_autoscaling_group.elb.*.id to obtain a list of the attributes across all instances

--- modules/aws/compute/asg/asg.tf
+++ modules/aws/compute/asg/asg.tf
@@ -88,9 +88,9 @@ resource "aws_autoscaling_group" "alb" {
 }
 
 output "name" {
-  value = "${element(concat(aws_autoscaling_group.alb.name, aws_autoscaling_group.elb.name), var.use_elb)}"
+  value = "${element(concat(aws_autoscaling_group.alb.*.name, aws_autoscaling_group.elb.*.name), var.use_elb)}"
 }
 
 output "id" {
-  value = "${element(concat(aws_autoscaling_group.alb.id, aws_autoscaling_group.elb.id), var.use_elb)}"
+  value = "${element(concat(aws_autoscaling_group.alb.*.id, aws_autoscaling_group.elb.*.id), var.use_elb)}"
 }
